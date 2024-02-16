b6920814a421d220ea6b401dc8f0a6b8926e4a2c
Stefano <snebo54@hotmail.com>
2024-02-17 00:17:19 +0100

    fix variable name

--- chess_pieces.rb
+++ chess_pieces.rb
@@ -29,12 +29,12 @@ class Pawn
     if one_step == '--' && !one_step.nil?
       @possible_moves << [curr_loc[0] + move_index, curr_loc[1]]
     end
-    digonal_left = board[curr_loc[0] + move_index][curr_loc[1] - move_index]
+    diagonal_left = board[curr_loc[0] + move_index][curr_loc[1] - move_index]
     if !diagonal_left.nil? && !diagonal_left == '--' && !diagonal_left.color == @color
       @possible_moves << [curr_loc[0] + move_index, curr_loc[1] - move_index]
     end
     
-    digonal_right = board[curr_loc[0] + move_index][curr_loc[1] + move_index]
+    diagonal_right = board[curr_loc[0] + move_index][curr_loc[1] + move_index]
     if !diagonal_right.nil? && !diagonal_right == '--' && !diagonal_right.color == @color
       @possible_moves << [curr_loc[0] + move_index, curr_loc[1] + move_index]
     end
