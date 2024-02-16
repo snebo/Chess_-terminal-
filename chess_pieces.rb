bb8a25365eeb3b2038626d08d2c6480d45742239
Stefano <snebo54@hotmail.com>
2024-02-17 00:16:21 +0100

    add prototype pawn moves

--- chess_pieces.rb
+++ chess_pieces.rb
@@ -12,6 +12,34 @@ class Pawn
     @value = color == 'black' ? "\u2659" : "\u265F"
   end
   # should have a move function.. on the has moved should be affect moveset
+  def possible_moves(board)
+    # one step foward if space is empty
+    # two steps foward, if has not moved and space is empty
+    # check if diagonal is not empty and has an opposing piece
+    @possible_moves = []
+    move_index = @color == 'white' ? 1 : -1
+
+    two_step = board[curr_loc[0] + move_index + move_index][curr_loc[1]]
+    if two_step == '--' && !two_step.nil? && @has_moved == false
+      @possible_moves << [curr_loc[0] + move_index + move_index, curr_loc[1]]
+      @has_moved = true
+    end
+
+    one_step = board[curr_loc[0] + move_index][curr_loc[1]]
+    if one_step == '--' && !one_step.nil?
+      @possible_moves << [curr_loc[0] + move_index, curr_loc[1]]
+    end
+    digonal_left = board[curr_loc[0] + move_index][curr_loc[1] - move_index]
+    if !diagonal_left.nil? && !diagonal_left == '--' && !diagonal_left.color == @color
+      @possible_moves << [curr_loc[0] + move_index, curr_loc[1] - move_index]
+    end
+    
+    digonal_right = board[curr_loc[0] + move_index][curr_loc[1] + move_index]
+    if !diagonal_right.nil? && !diagonal_right == '--' && !diagonal_right.color == @color
+      @possible_moves << [curr_loc[0] + move_index, curr_loc[1] + move_index]
+    end
+    
+  end
 end
 
 class Rook
